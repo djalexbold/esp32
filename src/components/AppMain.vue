df91d8e098de7e3be8242c95458deb1e5b7599e3
alexbold <djalexbold@gmail.com>
2023-12-19 01:14:50 +0300

    fix css

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,19 +1,27 @@
 <template>
   <div id="main-form">
     <div id="form">
-      <h2>Sensor 1</h2>
+      <h4>Sensor 1</h4>
     </div>
     <div id="form">
-      <h2>Sensor 2</h2>
+      <h4>Sensor 2</h4>
+      <ul>
+        <li>temperature: 34</li>
+        <li>Humidity:33</li>
+        <li>Pressure:4</li>
+      </ul>
     </div>
     <div id="form">
-      <h2>Sensor 3</h2>
+      <h4>Sensor 3</h4>
     </div>
     <div id="form">
-      <h2>Sensor 4</h2>
+      <h4>Sensor 4</h4>
     </div>
     <div id="form">
-      <h2>Sensor 5</h2>
+      <h4>Sensor 5</h4>
+      <ul>
+        <li>temperature: 34</li>
+      </ul>
     </div>
   </div>
 </template>
@@ -32,27 +40,40 @@ export default {
 
 
 <style>
-#main-form{
+#main-form {
   display: flex;
   flex-direction: row;
   align-items: flex-start;
   flex-flow: nowrap;
-  justify-content: center;
+  justify-content: left;
   align-content: space-evenly;
   flex-wrap: wrap;
-
 }
+
+#form > h4 {
+  text-align: center;
+}
+
+#form > ul {
+  background-color: blueviolet;
+  list-style-type: none;
+  width: 100%;
+  padding-left: 0;
+}
+
 #form {
-  display: inline-block;
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  justify-content: space-evenly;
   width: 100px;
   height: 75px;
   margin: 5px;
+  padding: 3px;
   border-radius: 10px;
   background-color: #8efffa;
   border: 3px solid #009f9f;
-  box-shadow:
-      inset 0 -3em 3em rgba(0, 0, 0, 0.1),
-      0.3em 0.3em 1em rgba(0, 0, 0, 0.3);
-
+  box-shadow: inset 0 -3em 3em rgba(0, 0, 0, 0.1),
+  0.3em 0.3em 1em rgba(0, 0, 0, 0.3);
 }
 </style>
